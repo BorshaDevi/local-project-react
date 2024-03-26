7cef966e3a1324eda572e2e2a086831745817cab
Borshadevi <borshadevi27@gmail.com>
2024-03-26 19:55:56 +0600

    add new file

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1 className='bg-green-500'>Vite + React</h1>
+      <button className="btn btn-link">Link</button>
+      
+    </>
+  )
+}
+
+export default App
